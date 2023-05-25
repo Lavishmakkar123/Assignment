42162711ab11722b9697fa4f48a3aa4939fa383f
Lavishmakkar123 <Lavishmakkar98@gmail.com>
2023-05-24 20:39:51 -0400

    Added

---
+++ src/Assignments/Assignment2.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+public class Assignment2 {
+    public static void main(String[] args) {
+        System.out.println("jfhdvbk");
+        System.out.println("jfhdvbk");
+        System.out.println("jfhdvbk");
+        System.out.println("jfhdvbk");
+        System.out.println("jfhdvbk");
+        System.out.println("jfhdvbk");
+    }
+}
